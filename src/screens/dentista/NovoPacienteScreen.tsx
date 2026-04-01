bfe98aa6e4519f75097e16dde9c9646264aff653
Catrevaap01 <catrevaap01@gmail.com>
2026-04-01 04:10:34 +0100

    ok

--- src/screens/dentista/NovoPacienteScreen.tsx
+++ src/screens/dentista/NovoPacienteScreen.tsx
@@ -211,7 +211,6 @@ const NovoPacienteScreen: React.FC<Props> = ({ navigation: propNavigation }) => 
             onPress={handleCreatePaciente}
             loading={loading}
             style={styles.btn}
-            icon="qr-code-outline"
           />
         </View>
       </ScrollView>
